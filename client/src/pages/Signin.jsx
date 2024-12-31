5238b6e437fa123a940c33679c28798a7692baa9
Sameena-06 <sameenaaktharm@gmail.com>
2024-12-31 15:08:29 +0530

    add redux persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -7,39 +7,47 @@ import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSli
 export default function Signin() {
   const [formData, setFormData]=useState({})
   const {loading, error}=useSelector((state)=> state.user);
+
+  const currentUser = useSelector((state) => state.user); 
+
   const navigate=useNavigate();
   const dispatch=useDispatch();
+
   const handleChange=(e)=>{
     setFormData({
       ...formData,
       [e.target.id]:e.target.value
     })
-  }
+  };
+
   const handleSubmit=async (e)=>{
     e.preventDefault();
     try{
       dispatch(signInStart());
-    const res=await fetch('/api/auth/signin',
-      {
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json'
-        },
-        body:JSON.stringify(formData)
+
+        const res=await fetch('/api/auth/signin',
+        {
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify(formData)
+        }
+      );
+
+      const data=await res.json();
+      if(data.success===false){
+        dispatch(signInFailure(data.message));
+        return;
       }
-    );
-    const data=await res.json();
-    if(data.success===false){
-      dispatch(signInFailure(data.message));
-      return;
-    }
-    console.log('Data received:', data);
-    dispatch(signInSuccess(data.user));
-    navigate('/');
-    } catch(error){
-      dispatch(signInFailure(error.message));
-    }
-  };
+      dispatch(signInSuccess(data));
+
+      navigate('/');
+      } catch(error){
+        dispatch(signInFailure(error.message));
+      }
+    };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
